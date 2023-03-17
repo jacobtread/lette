77351bd7f28f3ee58137e581f07b458c0ec58ccc
Jacobtread <jacobtread@gmail.com>
2023-03-17 16:32:43 +1300

    Stille deciding on a file format

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,46 @@
+use std::collections::HashMap;
 
+pub struct LetteDocument<'a> {
+    /// Styling groups
+    style_groups: HashMap<&'a str, StyleGroup<'a>>,
+}
+
+pub struct Style<'a> {
+    key: &'a str,
+    value: &'a str,
+}
+
+pub struct StyleGroup<'a> {
+    styles: Vec<Style<'a>>,
+}
+
+pub enum ElementType {
+    Preview,
+    Container,
+    Column,
+    Section,
+    Button,
+    Heading,
+    Text,
+}
+
+pub struct Element<'a> {
+    ///
+    ty: ElementType,
+    /// Element arguments
+    args: Args<'a>,
+    /// Styles for the element
+    styles: StyleGroup<'a>,
+    /// Attributes on the element
+    attributes: Attributes<'a>,
+    /// Child elements
+    children: Vec<Box<Element<'a>>>,
+}
+
+pub struct Args<'a> {
+    values: Vec<&'a str>,
+}
+
+pub struct Attributes<'a> {
+    values: HashMap<&'a str, &'a str>,
+}
